fe2259be437a15ad97a77c30c9f5fc3a18dbec18
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-05-11 11:02:10 +0200

    .

--- KompletteringOlleBW/Program.cs
+++ KompletteringOlleBW/Program.cs
@@ -1,8 +1,21 @@
 ﻿using System;
-Hello();
-Console.ReadLine();
+int Lives = 3;
+bool Correct = false;
 
+while (Lives > 0)
+{
+    Hello();
+    Console.ReadLine();
 
+    Console.WriteLine("What is 2+2?");
+    string Choise = Console.ReadLine();
+    Choise = Choise.ToString();
+    Choise = Choise.ToLower();
+    if (Choise = "4" || Choise = "four")
+    {
+        True();
+    }
+}
 
 
 
@@ -10,4 +23,17 @@ Console.ReadLine();
 static void Hello()
 {
     Console.WriteLine("Hello and welcome to my quiz!");
+    Console.WriteLine("Press Enter to continue.");
+}
+static void True()
+{
+    Console.WriteLine("That was Correct!");
+    Console.WriteLine("Press Enter to continue.");
+    Console.ReadLine();
+}
+static void False()
+{
+    Console.WriteLine("That was Incorrect!");
+    Console.WriteLine("Press Enter to Try Again.");
+    Console.ReadLine();
 }
