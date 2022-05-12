755824274834c5baa46e22b8547293cc0b0dfc2a
OlleBW <91120009+OlleBW@users.noreply.github.com>
2022-05-12 18:18:46 +0200

    .

--- KompletteringOlleBW/Program.cs
+++ KompletteringOlleBW/Program.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 
 int Lives = 3;
-List<string> Choises = new List<string>();
 
+bool Alive = true;
 bool Question1 = false;
 bool Question2 = false;
 
@@ -12,7 +12,7 @@ Console.ReadLine();
 Console.Clear();
 
 
-while (Lives > 0)
+while (Alive == true)
 {
     // Code runs until you get Question 1 right
     while (Question1 == false)
@@ -28,6 +28,51 @@ while (Lives > 0)
             Correct();
             Question1 = true;
         }
+        // Each time you answer incorrectly you will lose 1 life
+        else
+        {
+            Lives = Lives - 1;
+            Console.Clear();
+            Console.WriteLine($"You now have {Lives} Lives left");
+            False();
+        }
+        // Ends quiz if you run out of lives
+        if (Lives == 0)
+        {
+            Console.Clear();
+            Console.WriteLine("You died, Shame on you!");
+            Console.ReadLine();
+            Alive = false;
+            Question1 = true;
+            Question2 = true;
+        }
+    }
+
+    List<string> Choises = new List<string>();
+    Choises.Add("73");
+    Choises.Add("21");
+    Choises.Add("10");
+
+    // Code runs until you get Question 2 right
+    while (Question2 == false)
+    {
+        Console.WriteLine("What is 7x3?");
+
+        Console.WriteLine(Choises[0] + " A");
+        Console.WriteLine(Choises[1] + " B");
+        Console.WriteLine(Choises[2] + " C");
+
+        string Choise2 = Console.ReadLine();
+        Choise2 = Choise2.ToLower();
+
+        if (Choise2 == "b")
+        {
+            Console.Clear();
+            Correct();
+            Victory();
+            Question2 = true;
+            Alive = false;
+        }
         else
         {
             Lives = Lives - 1;
@@ -40,34 +85,11 @@ while (Lives > 0)
             Console.Clear();
             Console.WriteLine("You died, Shame on you!");
             Console.ReadLine();
-            Question1 = true;
+            Alive = false;
             Question2 = true;
         }
     }
-
-    Choises.Add("73");
-    Choises.Add("21");
-    Choises.Add("10");
-
-    while (Question2 == false)
-    {
-        Console.WriteLine("What is 7x3?");
-
-        Console.WriteLine(Choises[0] + " A");
-        Console.WriteLine(Choises[1] + " B");
-        Console.WriteLine(Choises[2] + " C");
-
-        string Choise2 = Console.ReadLine();
-        Choise2 = Choise2.ToLower();
-
-        if (Choise2 == "B")
-        {
-            Correct();
-
-        }
-    }
 }
-
 
 // Writes an introduction to the quiz
 static void Hello()
@@ -82,6 +104,7 @@ static void Correct()
     Console.WriteLine("That was Correct!");
     Console.WriteLine("Press Enter to continue.");
     Console.ReadLine();
+    Console.Clear();
 }
 
 // Writes out specific message if player is incorrect
@@ -89,4 +112,11 @@ static void False()
 {
     Console.WriteLine("That was Incorrect!");
     Console.WriteLine("Try Again!");
+}
+
+static void Victory()
+{
+    Console.WriteLine("Congratulations! You Won!");
+    Console.WriteLine("Press ENTER to quit.");
+    Console.ReadLine();
 }
